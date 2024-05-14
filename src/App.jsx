f07b0716db5f1903712321d74dfd828441508113
Fiona Belioute <nona.belioute@gmail.com>
2024-05-14 16:15:11 +0200

    MAJ des disfonctionnements - Attention FontAwesome a remettre

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import Error from './pages/Error/Error'
+import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import Home from './pages/Home/Home.jsx';
+import About from './pages/About/About.jsx';
+import Error from './pages/Error/Error.jsx';
 import "./app.scss"
 
 
